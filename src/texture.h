e42608762b498e83745c4acb8d6e5f530c4ea558
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2006-04-05 04:39:30 +0000

    - Fixed load_image() prototype

--- src/texture.h
+++ src/texture.h
@@ -6,16 +6,18 @@
  *   (c) 2006 Beau V.C. Bellamy (beau@stellarnetservices.net)              *
  ***************************************************************************/
 
+#include "SDL/SDL_opengl.h"
+
 typedef struct {
     int width;
     int height;
     unsigned char *data;
 } textureImage;
 
-void load_image  (char *file);
-void load_skybox (void);
-void skybox (void);
-int  loadBMP(const char *filename, textureImage *texture);
-bool getBitmapImageData(const char *pFileName, textureImage *pImage );
+GLuint load_image  (const char *file);
+void   load_skybox (void);
+void   skybox (void);
+int    loadBMP(const char *filename, textureImage *texture);
+bool   getBitmapImageData(const char *pFileName, textureImage *pImage );
 
 #endif /* TEXTURE_H */
