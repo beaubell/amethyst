d4b69739e963df3a01f81321f69c0af43f522b55
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2006-04-09 04:30:20 +0000

    *** empty log message ***

---
+++ src/config_xml.cpp
@@ -0,0 +1,7 @@
+/***************************************************************************
+ *  Put info here                                                          *
+ *  (c) 2006 Beau V.C. Bellamy (beau@stellarnetservices.net)               *
+ ***************************************************************************/
+
+#include <libxml/tree.h>
+#include <libxml/parser.h>
