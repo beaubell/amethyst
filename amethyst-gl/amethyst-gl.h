58b41d965d02efabb88f41cf548259e2520b872e
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2008-10-13 07:25:37 +0000

    - forgot to add the class header.

---
+++ amethyst-gl/amethyst-gl.h
@@ -0,0 +1,35 @@
+#ifndef AMETHYST_CLIENT_AMETHYSTGL_H
+#define AMETHYST_CLIENT_AMETHYSTGL_H
+/***********************************************************************
+ Amethyst-GL
+  - Main class declarations declarations
+
+ Authors (c):
+ 2008-2008 Beau V.C. Bellamy (beau@stellarnetservices.net)
+
+ $Revision$
+ $LastChangedDate$
+ $LastChangedBy$
+ ***********************************************************************/
+
+#include "lib/manifest.h"
+
+namespace amethyst
+{
+namespace client
+{
+
+class amethyst_gl
+{
+   public:
+    amethyst_gl(const std::string &path_root);
+
+   private:
+    lib::filemanifest manifest_;
+
+};
+
+} // namespace client
+} // namespace amethyst
+
+#endif // AMETHYST_CLIENT_AMETHYSTGL_H
