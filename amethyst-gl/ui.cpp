359376007133f967a640c5683d64badbe6f620c4
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2008-10-25 02:26:50 +0000

    * laptop sync - working on texture fonts.

--- amethyst-gl/ui.cpp
+++ amethyst-gl/ui.cpp
@@ -30,8 +30,22 @@ UI::UI(const std::string &fontfile)
     else
       std::cout << "Font: %s loaded" << fontpath << std::endl;
 
-    font->FaceSize( 13);
+    static GLuint textureID;
 
+    font->FaceSize( 18);
+    //font->Depth(20);
+    font->UseDisplayList(true);
+
+    //font->CharMap(ft_encoding_unicode);
+/*
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 4, 4, 0, GL_RGB, GL_FLOAT, texture);
+*/
 }
 
 UI::~UI()
@@ -44,18 +58,56 @@ UI::~UI()
 
 void UI::render(void)
 {
-    gluOrtho2D(0, 600, 0, 800);
-    glMatrixMode(GL_MODELVIEW);
+    glPushAttrib(GL_ALL_ATTRIB_BITS);
+    glPushMatrix();
 
+    int w_win = Global.screen_y; int h_win = Global.screen_x;
+
+    // Set camera
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluLookAt( 0.0, 0.0, (float)h_win/2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+
+    float light1_ambient[4]  = { 1.0, 1.0, 1.0, 1.0 };
+    float light1_diffuse[4]  = { 1.0, 0.9, 0.9, 1.0 };
+    float light1_specular[4] = { 1.0, 0.7, 0.7, 1.0 };
+    float light1_position[4] = { -1.0, 1.0, 1.0, 0.0 };
+    glLightfv(GL_LIGHT1, GL_AMBIENT,  light1_ambient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE,  light1_diffuse);
+    glLightfv(GL_LIGHT1, GL_SPECULAR, light1_specular);
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+    glEnable(GL_LIGHT1);
+
+    float front_emission[4] = { 0.3, 0.2, 0.1, 0.0 };
+    float front_ambient[4]  = { 0.2, 0.2, 0.2, 0.0 };
+    float front_diffuse[4]  = { 0.95, 0.95, 0.8, 0.0 };
+    float front_specular[4] = { 0.6, 0.6, 0.6, 0.0 };
+    glMaterialfv(GL_FRONT, GL_EMISSION, front_emission);
+    glMaterialfv(GL_FRONT, GL_AMBIENT, front_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, front_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, front_specular);
+    glMaterialf(GL_FRONT, GL_SHININESS, 16.0);
+    glColor4fv(front_diffuse);
+
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+    glColorMaterial(GL_FRONT, GL_DIFFUSE);
+    glEnable(GL_COLOR_MATERIAL);
+
+    glEnable(GL_LIGHTING);
 
     glEnable( GL_TEXTURE_2D);
-    //glDisable( GL_DEPTH_TEST);
+    glDisable( GL_DEPTH_TEST);
 
+    glNormal3f( 0.0, 0.0, 1.0);
     glColor3f( 1.0, 1.0, 1.0);
-    glRasterPos2i( 20 , 20);
-    font->Render("TEST TEST TEST");
 
+    glRasterPos2f(-0.40f, 0.35f);
+
+    font->Render("ABCDEFGH");
+
+    glPopMatrix();
+    glPopAttrib();
 }
 
 //void UI::add();
