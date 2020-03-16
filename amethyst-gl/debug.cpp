74b34cd76e5cb2752476b1875e53c765461b8a01
Beau V.C. Bellamy <bbellamy.ctr@diux.mil>
2020-03-15 23:12:43 -0700

    Remove unused printing functions

--- amethyst-gl/debug.cpp
+++ amethyst-gl/debug.cpp
@@ -3,11 +3,7 @@
   - Debugging helper functions
 
  Authors (c):
- 2006-2008 Beau V.C. Bellamy (beau@stellarnetservices.net)
-
- $Revision$
- $LastChangedDate$
- $LastChangedBy$
+ 2006-2020 Beau V.C. Bellamy (bellamy.beau@gmail.com)
  ***********************************************************************/
 
 #include "debug.h"
@@ -26,35 +22,6 @@
 
 namespace amethyst {
 namespace client {
-
-using lib::Cartesian_Vector;
-
-void print_vector(const char *title, const Cartesian_Vector &vector) {
-
-       printf("%s CV: x:%#1.16lE y:%#1.16lE z:%#1.16lE\n", title, vector.x, vector.y, vector.z);
-       }
-
-
-void print_camera (Cartesian_Vector &real_pos,
-                   Cartesian_Vector &real_view,
-                   Cartesian_Vector &real_up)
-{
-        //print_vector("ship position", position);
-        print_vector("real_pos     ", real_pos);
-        print_vector("real_view    ", real_view);
-        print_vector("real_up      ", real_up);
-        //printf("RAW  SQUARE: %f \n", phys_distance(raw_view, raw_up));
-        //printf("REAL SQUARE: %f \n", phys_distance(real_view, real_up));
-}
-
-void print_object(Object &obj)
-{
-    print_vector("Location ", obj.location);
-    print_vector("Velocity ", obj.velocity);
-    print_vector("Accelera ", obj.acceleration);
-    printf("Attitude, W: %f, X: %f, Y: %f, Z: %f\n", obj.attitude.w, obj.attitude.x, obj.attitude.y, obj.attitude.z);
-}
-
 
 void print_trace(void)
 {
--- amethyst-gl/debug.h
+++ amethyst-gl/debug.h
@@ -6,29 +6,12 @@
   - Debugging helper functions
 
  Authors (c):
- 2006-2008 Beau V.C. Bellamy (beau@stellarnetservices.net)
-
- $Revision$
- $LastChangedDate$
- $LastChangedBy$
+ 2006-2020 Beau V.C. Bellamy (bellamy.beau@gmail.com)
  ***********************************************************************/
-
-#include "lib/vector.h"
-#include "lib/object.h"
 
 namespace amethyst {
 namespace client {
 
-using lib::Object;
-
-#if 0
-void print_vector(char *title, const Cartesian_Vector &vector);
-void print_camera(Cartesian_Vector &real_pos,
-                  Cartesian_Vector &real_view,
-                  Cartesian_Vector &real_up);
-#endif
-
-void print_object(Object &obj);
 
 void print_trace(void);
 
