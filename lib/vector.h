7ffe94eb04a8daf2e330cab58b956556a6208eac
Beau V.C. Bellamy <bellamy.beau@gmail.com>
2020-04-05 23:54:22 -0700

    Switch <ostream> to <iosfwd> in headers

--- lib/vector.h
+++ lib/vector.h
@@ -7,7 +7,7 @@
  2006-2020 Beau V.C. Bellamy (bellamy.beau@gmail.com)
  ***********************************************************************/
 
-#include <ostream>
+#include <iosfwd>
 #include "yaml-cpp/node/node.h"
 
 namespace amethyst::lib {
